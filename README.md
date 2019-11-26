e0d2c02a5419637faea3a6fa147f05192afb8f2f
Andrii Guselietov <guseletov@gmail.com>
2019-11-26 13:56:39 +0100

    Demo code

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # tf-for-each
-Sampel repo, Terraform, for-each
+Sample repo, Terraform, for-each
+
+
+# TODO
+- [ ] create readme
+
+# DONE
+- [x] make demo code
---
+++ main.tf
@@ -0,0 +1,19 @@
+variable "pets_activity_list" {
+  default = { 
+    "duck" : "is roasted", 
+    "chicken" : "is fried",
+    "pony" : "constantly eats"
+    }
+}
+
+resource "null_resource" "echo" {
+  for_each = var.pets_activity_list 
+  
+  triggers = {
+    time = "${each.value}"
+  }
+
+  provisioner "local-exec"  {
+    command = "echo ${each.value}"
+  }
+}
